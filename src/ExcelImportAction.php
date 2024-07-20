3bc585a47490dbfb01b57de1820155b82eccc125
89 <eighty9nine@gmail.com>
2024-07-20 20:30:17 +0300

    Adds before import and after import functions

--- src/ExcelImportAction.php
+++ src/ExcelImportAction.php
@@ -7,6 +7,8 @@ use Filament\Actions\Action;
 use Filament\Forms\Components\FileUpload;
 use Maatwebsite\Excel\Facades\Excel;
 
+use function Pest\Laravel\call;
+
 class ExcelImportAction extends Action
 {
     protected string $importClass = DefaultImport::class;
@@ -16,6 +18,14 @@ class ExcelImportAction extends Action
     protected ?string $disk = null;
 
     protected string | Closure $visibility = 'public';
+
+    protected ?Closure $beforeImportClosure = null;
+
+    protected ?Closure $afterImportClosure = null;
+
+    protected array $acceptedFileTypes = ['application/vnd.ms-excel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'text/csv'];
+
+    protected bool $storeFiles = false;
 
     public function use(string $class = null, ...$attributes): static
     {
@@ -37,6 +47,20 @@ class ExcelImportAction extends Action
         return $this;
     }
 
+    public function acceptedFileTypes(array $types): static
+    {
+        $this->acceptedFileTypes = $types;
+
+        return $this;
+    }
+
+    public function storeFiles(bool $storeFiles): static
+    {
+        $this->storeFiles = $storeFiles;
+
+        return $this;
+    }
+
     public static function getDefaultName(): ?string
     {
         return 'import';
@@ -53,10 +77,25 @@ class ExcelImportAction extends Action
         return $this;
     }
 
+    public function beforeImport(Closure $closure): static
+    {
+        $this->beforeImportClosure = $closure;
+
+        return $this;
+    }
+
+    public function afterImport(Closure $closure): static
+    {
+        $this->afterImportClosure = $closure;
+
+        return $this;
+    }
+
     protected function getDefaultForm(): array
     {
         return [
             FileUpload::make('upload')
+                ->acceptedFileTypes($this->acceptedFileTypes)
                 ->label(function ($livewire) {
                     if (! method_exists($livewire, 'getTable')) {
                         return __('Excel Data');
@@ -65,6 +104,7 @@ class ExcelImportAction extends Action
                     return str($livewire->getTable()->getPluralModelLabel())->title() . ' ' . __('Excel Data');
                 })
                 ->default(1)
+                ->storeFiles($this->storeFiles)
                 ->disk($this->getDisk())
                 ->visibility($this->visibility)
                 ->columns()
@@ -100,12 +140,17 @@ class ExcelImportAction extends Action
     private function importData(): Closure
     {
         return function (array $data, $livewire): bool {
+            if(is_callable($this->beforeImportClosure)){
+                call_user_func($this->beforeImportClosure, $data, $livewire);
+            }
             $importObject = new $this->importClass(
                 method_exists($livewire, 'getModel') ? $livewire->getModel() : null,
                 ...$this->importClassAttributes
             );
             Excel::import($importObject, $data['upload']);
-
+            if(is_callable($this->afterImportClosure)){
+                call_user_func($this->afterImportClosure, $data, $livewire);
+            }
             return true;
         };
     }
